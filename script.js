a8617184a2a53de38649fcab570c9306d34b936f
BIPIN <timilsinabipin20@gmail.com>
2023-12-15 13:16:59 +0545

    dunno what this commit is for changes wore not staged thats why

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let playerWinCount = 0,
   computerWinCount = 0;
-// let choices={"rock","papers","scissors"};
 
 let getComputerChoice = () => {
   let randomNumber = Math.floor(Math.random() * 3);
@@ -15,16 +14,14 @@ let getComputerChoice = () => {
       compuChoice = "SCISSORS";
       break;
   }
-  // compuChoice = "paper";
   compuChoice = compuChoice.toLowerCase();
   console.log(`Computer Choose ${compuChoice}`);
   return compuChoice;
 };
 function getRandomChoice() {}
 
+//function for 1 round game
 function playRound(playerSelection, computerSelection) {
-  // your code here!
-
   if (playerSelection == computerSelection) {
     return "::::Tie:::";
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
@@ -50,12 +47,8 @@ function playRound(playerSelection, computerSelection) {
     return "You lose: Paper beats Rock";
   }
 }
-// let roundCounter = () => {
-//   for (i = 1; i <= 5; i++) {
-//     console.log(`Round ${i}`);
-//   }
-// };
 
+//function for more than one round game and score Card
 let game = () => {
   console.log("lets play Five Rounds".toUpperCase());
   for (i = 1; i <= 5; i++) {
@@ -63,19 +56,15 @@ let game = () => {
     let playerSelection = askPlayerSelection();
 
     let computerSelection = getComputerChoice();
-    var delayInMilliseconds = 5000; //1 second
 
-    // setTimeout(function () {
-      //your code to be executed after 1 second
-      console.log(playRound(playerSelection, computerSelection));
-    // }, delayInMilliseconds);
+    console.log(playRound(playerSelection, computerSelection));
     console.log(
       `Win Count: Player-${playerWinCount} Computer-${computerWinCount} `
     );
 
     console.log("\n");
   }
-
+  //its for checking who won
   playerWinCount > computerWinCount
     ? console.log(`congrats! You win`.toUpperCase())
     : playerWinCount < computerWinCount
